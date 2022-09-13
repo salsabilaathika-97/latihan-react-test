42d5ff7bbab16cae8956e606db9a73985dad12ca
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-13 21:31:09 +0800

    add testing sum number

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 const multiple = (a,b) => a * b;
+const sum = (a,b) => a + b;
 
 test("Hasil testing perkalian", () => {
   const perkalian = multiple(3,2);
   expect(perkalian).toBe(6);
+});
+
+test("Hasil testing penjumlahan",() => {
+  const penjumlahan = sum(3,2);
+  expect(penjumlahan).toBe(5);
 });
