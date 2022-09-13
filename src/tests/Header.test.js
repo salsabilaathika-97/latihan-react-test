0e55d657405980ca1a52f1e488465cdb998dbc41
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-13 22:43:13 +0800

    add configure in header test to fix error

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+import Enyzme, { shallow } from "enzyme";
 import Header from "../components/Header";
+import Adapter from 'enzyme-adapter-react-16';
+
+Enyzme.configure({adapter: new Adapter()});
 
 test("Header component expect", () => {
     const wrapper = shallow(<Header />)
     expect(wrapper).toMatchSnapshot();
-});
+})
